39c2c52248e7e2a7cb917497cd08c0f6f3f8c377
d22698 <bbam2503@gmail.com>
2024-03-04 10:05:53 -0300

    init

--- src/movie_data/Leitor.java
+++ src/movie_data/Leitor.java
@@ -6,50 +6,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Leitor {
-    private FileReader fileReader;
-    private BufferedReader bufferedreader;
     private String caminhoArquivo;
-    private int numeroColunas;
+
     List<String> preenchimento;
 
-    public Leitor(String caminhoArquivo, int numeroColunas){
+    public Leitor(String caminhoArquivo){
         setCaminhoArquivo(caminhoArquivo);
-        setNumeroColunas(numeroColunas);
     }
-
 
     public void setCaminhoArquivo(String caminhoArquivo) {
         this.caminhoArquivo = caminhoArquivo;
     }
 
-    public void setNumeroColunas(int numeroColunas) {
-        this.numeroColunas = numeroColunas;
-    }
-
-    public FileReader getFileReader() {
-        return fileReader;
-    }
-
-    public BufferedReader getBufferedreader() {
-        return bufferedreader;
-    }
-
-    public String getCaminhoArquivo() {
-        return caminhoArquivo;
-    }
-
-    public int getNnumeroColunas() {
-        return numeroColunas;
-    }
-
-    public List<String> getPreenchimento() {
-        return preenchimento;
-    }
-
     public void ler() {
         try {
-            fileReader = new FileReader(caminhoArquivo);
-            bufferedreader = new BufferedReader(fileReader);
+            FileReader fileReader = new FileReader(caminhoArquivo);
+            BufferedReader bufferedreader = new BufferedReader(fileReader);
             List<String> linhas = new ArrayList<>();
             String linha;
             preenchimento = new ArrayList<>();
@@ -57,8 +29,10 @@ public class Leitor {
 
             while ((linha = bufferedreader.readLine()) != null){
                 linhas.add(linha);
-                String[] palavras = linhas.get(cont).split(";");
-                preenchimento.addAll(Arrays.asList(palavras));
+                 if (cont > 0){
+                    String[] palavras = linhas.get(cont).split(";");
+                    preenchimento.addAll(Arrays.asList(palavras));
+                }
                 cont++;
             }
 
@@ -70,6 +44,4 @@ public class Leitor {
             System.out.println("IOExcepition.");
         }
     }
-
-
 }
--- src/movie_data/Main.java
+++ src/movie_data/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Leitor actors;
-        actors = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\actors.CSV", 4);
+        actors = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\actors.CSV");
         Leitor movies;
-        movies = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\movies.CSV", 4);
+        movies = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\movies.CSV");
         Leitor role;
-        role = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\roles.CSV", 3);
+        role = new Leitor("C:\\Users\\d22698\\OneDrive - FUNDAÇÃO MOVIMENTO DIREITO E CIDADANIA\\Área de Trabalho\\Documentos\\BANCO DE DADOS\\BANCO DE DADOS - Exercícios\\Ex01\\01-Arquivos CSV\\CSV\\roles.CSV");
 
         actors.ler();
         movies.ler();
@@ -23,24 +23,24 @@ public class Main {
         lista3 = new Role[(role.preenchimento.toArray().length) / 4];
 
         for (Actor item : lista) {
-            for (int aux = 0; aux < actors.preenchimento.toArray().length; aux++) {
+            for (int aux = 0; aux < lista.length; aux++) {
                 if (aux == 0 | aux % 4 == 0) {
                     //id
-                    item.setId(Integer.parseInt(actors.preenchimento.get(aux)));
+                    lista[aux].setId(Integer.parseInt(actors.preenchimento.get(aux)));
                 } else if (aux == 1 | (aux - 1) % 4 == 0) {
                     //nome
-                    item.setFirst_name(actors.preenchimento.get(aux));
+                    lista[aux].setFirst_name(actors.preenchimento.get(aux));
                 } else if (aux == 2 | (aux - 2) % 4 == 0) {
                     //sobrenome
-                    item.setLast_name(actors.preenchimento.get(aux));
+                    lista[aux].setLast_name(actors.preenchimento.get(aux));
                 } else {
                     //genero
-                    item.setGender(actors.preenchimento.get(aux));
+                    lista[aux].setGender(actors.preenchimento.get(aux));
                 }
             }
         }
         for (Movie item : lista2) {
-            for (int aux = 0; aux < movies.preenchimento.toArray().length; aux++) {
+            for (int aux = 0; aux < lista2.length; aux++) {
                 if (aux == 0 | aux % 4 == 0) {
                     //id
                     item.setId(Integer.parseInt(movies.preenchimento.get(aux)));
@@ -57,7 +57,7 @@ public class Main {
             }
         }
         for (Role item : lista3) {
-            for (int aux = 0; aux < role.preenchimento.toArray().length; aux++) {
+            for (int aux = 0; aux < lista3.length; aux++) {
                 if (aux == 0 | aux % 3 == 0) {
                     //ator id
                     item.setActorld(Integer.parseInt(role.preenchimento.get(aux)));
@@ -73,6 +73,7 @@ public class Main {
 
         System.out.println("Questão 1:");
         System.out.println("=========================================================================================");
+        System.out.println("first_name   last_name");
 
         for (Actor actor : lista) {
             if (actor.getGender().equalsIgnoreCase("F")) {
@@ -82,8 +83,11 @@ public class Main {
             }
         }
 
+        System.out.println();
+
         System.out.println("Questão 2:");
         System.out.println("=========================================================================================");
+        System.out.println("movie_name");
 
         for (Movie movie : lista2) {
             if (movie.getYear() > 1999) {
@@ -91,18 +95,21 @@ public class Main {
             }
         }
 
+        System.out.println();
+
         System.out.println("Questão 3:");
         System.out.println("=========================================================================================");
+        System.out.println("movie_name   role   actor_first_name   actor_last_name");
 
         for (Movie movie : lista2) {
             if (movie.getRank() > 6) {
-                System.out.println(movie.getName());
+                System.out.print(movie.getName() + "   ");
                 for (Role value : lista3) {
                     if (value.getMovieId() == movie.getId()) {
-                        System.out.println(role);
+                        System.out.print(role + "   ");
                         for (Actor actor : lista) {
                             if (actor.getId() == value.getActorId()){
-                                System.out.println(actor.getFirst_name());
+                                System.out.print(actor.getFirst_name());
                                 System.out.println(actor.getLast_name());
                             }
                         }
